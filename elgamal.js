d678cd91a5a376cd6b11bee37c6c29962583fce6
pieman353 <pieman353@aol.com>
2018-11-02 08:55:33 -0400

    Update elgamal.js

--- elgamal.js
+++ elgamal.js
@@ -1,9 +1,9 @@
 var btn = document.getElementById("button");
 button.addEventListener("click", function(event) {
-  document.getElementById("p").value() = "dick";
-  var p = document.getElementById("p").value().Number();
-  var g = document.getElementById("g").value().Number();
-  var q = document.getElementById("q").value().Number();
+  document.getElementById("p").value = "dick";
+  var p = document.getElementById("p").value.Number();
+  var g = document.getElementById("g").value.Number();
+  var q = document.getElementById("q").value.Number();
   var c = document.getElementById("c").replace("(","").replace(")","").split(",");
   var c1 = c[0].Number();
   var c2 = c[1].Number();
